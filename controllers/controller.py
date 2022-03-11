8986321715a21b7be4209a8da11160e77f8a4eeb
Jack <jackowendickson@gmail.com>
2022-03-11 11:17:32 +0000

    additional routes

--- controllers/controller.py
+++ controllers/controller.py
@@ -7,3 +7,14 @@ from models.player import Player
 def index():
     return render_template('index.html', title="let's play rock paper scissors")
     
+@app.route('/player1')
+def player_1_turn():
+    return render_template('player_1_turn.html')
+
+@app.route('/{player_1_choice}/player2')
+def player_2_turn():
+    return render_template('player_2_turn.html')
+
+@app.route('/{player_1_choice}/{player_2_choice}')
+def result():
+    return render_template('result.html')
