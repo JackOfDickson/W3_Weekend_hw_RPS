3b58b6a892f15b2d3cd68eebb516994d904da7f8
Jack <jackowendickson@gmail.com>
2022-03-13 19:11:09 +0000

    fixed the issues, players can now navigate through the pages

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,3 +1,4 @@
+from email.message import Message
 from flask import render_template
 from app import app
 from models.game import *
@@ -11,10 +12,10 @@ def index():
 def play_game():
     return render_template('play_game.html', player_1='Player 1', title='Play rock papaer scissors!')
 
-@app.route('/<player_1_choice>/player2turn')
+@app.route('/<player_1_choice>')
 def player2turn(player_1_choice):
     player_1 = Player("Player 1", player_1_choice)
-    return render_template('player2.html', player_2="Player 2")
+    return render_template('player2.html', player_2="Player 2", player_1_choice=player_1_choice)
 
 
 @app.route('/<player_1_choice>/<player_2_choice>')
@@ -24,7 +25,10 @@ def result(player_1_choice, player_2_choice):
     game = Game(player_1, player_2)
     winner = game.game_result().name
     if winner == None:
-        winner = "Nobody! It was a draw!"
+        message = "Nobody! It was a draw!"
+    else:
+        message = f"{winner} wins!"
 
-    return render_template('result.html', winner=winner, player_1=player_1,
+    return render_template('result.html', winner=winner,
+                           message=message, player_1=player_1,
                            player_2=player_2, title="and the winner is...")
